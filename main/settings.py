f554f0007a9a3e6bf5312e836910dd0a6ff54fe0
serjik-z <vdovicenco.z@gmail.com>
2022-09-22 00:12:11 +0300

    Third

--- main/settings.py
+++ main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapi',
     'rest_framework',
+    'imagekit',
 ]
 
 MIDDLEWARE = [
@@ -75,13 +76,22 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'Rest',
+    'USER': 'postgres',
+    'PASSWORD': 'blog',
+    'HOST': '127.0.0.1',
+    'PORT': '5432'
     }
 }
-
 
 
 
--- myapi/models.py
+++ myapi/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.urls import reverse
-
 
 # Create your models here.
+
 class Item(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField('Название', max_length=25)
